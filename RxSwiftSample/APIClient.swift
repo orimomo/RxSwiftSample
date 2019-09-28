4b6542c599c3838c18b1b6cfe1fd85cd5e25b2ea
momo <mmko5110@gmail.com>
2019-09-29 00:22:00 +0900

    MVVMに必要なファイルの作成

---
+++ RxSwiftSample/APIClient.swift
@@ -0,0 +1,9 @@
+//
+//  APIClient.swift
+//  RxSwiftSample
+//
+//  Created by 折田桃子 on 2019/09/29.
+//  Copyright © 2019 Momoko Orita. All rights reserved.
+//
+
+import Foundation
---
+++ RxSwiftSample/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  RxSwiftSample
+//
+//  Created by 折田桃子 on 2019/09/28.
+//  Copyright © 2019 Momoko Orita. All rights reserved.
+//
+
+import Foundation
