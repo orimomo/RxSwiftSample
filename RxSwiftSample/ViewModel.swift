00da1458ad9d46b5636c07c5eca516f89a73249b
momo <mmko5110@gmail.com>
2019-09-29 11:53:30 +0900

    API通信できるようにした

--- RxSwiftSample/ViewModel.swift
+++ RxSwiftSample/ViewModel.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import Alamofire
+
+class ViewModel {
+    func getItem() {
+        let URL = "https://qiita.com/api/v2/items"
+        
+        Alamofire.request(
+            URL,
+            method: .get,
+            parameters: [
+                "page": 1,
+                "per_page": 1,
+                "query": "qiita user:orimomo"
+            ])
+            .responseJSON { (response) in
+
+                switch response.result {
+                case .success(_):
+                    if let items = response.result.value as? [Any] {
+                        for item in items {
+                            if let JSON = item as? [String: Any] {
+                                let title = JSON["title"] as! String
+                                print(title)
+                            }
+                        }
+                    }
+                case .failure(_): break
+
+                }
+        }
+    }
+}
