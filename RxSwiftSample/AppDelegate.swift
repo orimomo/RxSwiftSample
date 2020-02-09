e10d6a02e3a67bcc4f321f27495148b2fad3c91f
orimomo <mmko5110@gmail.com>
2020-02-09 18:49:24 +0900

    ref

--- RxSwiftSample/AppDelegate.swift
+++ RxSwiftSample/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  RxSwiftSample
 //
-//  Created by 折田桃子 on 2019/09/28.
-//  Copyright © 2019 Momoko Orita. All rights reserved.
+//  Created by orimomo on 2019/09/28.
+//  Copyright © 2019 orimomo. All rights reserved.
 //
 
 import UIKit
